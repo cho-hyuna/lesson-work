9cb332206bc289da20578dd05d1a2e5bb858d38b
Cho Hyuna <hyuna-cho@ChonoMacBook-Pro.local>
2026-05-20 12:47:28 +0900

    feat: PHP第6章の配列複数型修正

--- php/lesson06/loops.php
+++ php/lesson06/loops.php
@@ -5,6 +5,7 @@ for($i=1; $i<=5; $i++){
 
 echo "\n";
 
-$number= [1,2,3,4,5];
-foreach($number as $number)
-echo $number . "\n";
+$numbers= [1,2,3,4,5];
+foreach($numbers as $number){
+    echo $number . "\n";
+    }
