6ee4a38aad945e8b36b3976d8f2c76ee4968869e
Cho Hyuna <hyuna-cho@ChonoMacBook-Pro.local>
2026-05-28 11:30:12 +0900

    lesson15aのstock_alert追加チャレンジ

--- php/lesson15a/stock_alert.php
+++ php/lesson15a/stock_alert.php
@@ -11,17 +11,27 @@ echo "在庫アラート\n";
 
 function isLowStock($stock){
     return $stock <= 5;
-}
+}//true, false 반환
+
+$minStock = null; //재고가 5개 이하인 상품들 중에서도 가장 적은 상품을 담을 바구니
+$lowestStock = 9999;
 
 foreach($products as $product){
-   if(isLowstock($product['stock'])){
-   echo "{$product['name']}: {$product['stock']}個\n";
 
-   if ($product['stock'] <= 3){
-    echo "対象件数: {$product['stock']}件\n";
-   }
+   if(isLowStock($product['stock'])){
+   echo "{$product['name']}: {$product['stock']}個\n";
+   // Notebook-4, Eraser-2
+
+   if($product['stock'] < $lowestStock){ // 4<999 // 2<4
+    $lowestStock = $product['stock']; // lowestStock=4 // lowestStock=2
+    $minStock = $product; //배열 통째로 저장. 
+    //$minStock = [["name" => "Notebook", "stock" => 4],["name" => "Eraser", "stock" => 2]]
+   };
    }
 }
+
+echo "対象件数: {$minStock['stock']}件\n";
+
 
 /*
 foreach($products as $product){
